db86596001c2b910533b305b5dc2243a75d68d71
Lucas Kwenda <133110595+lucaskwenda@users.noreply.github.com>
2025-05-10 18:27:13 +0100

    Update footer.js
    modando as rotas

--- js/footer.js
+++ js/footer.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Salva a página do chat como destino após o login
     sessionStorage.setItem('redirectAfterLogin', '../chat/chatIA.html');
     // Redireciona para a página de login
-    window.location.href = '../pages/login.html';
+    window.location.href = './pages/login.html';
   }
   // Função para redirecionar para o chat
   function redirectToChat() {
