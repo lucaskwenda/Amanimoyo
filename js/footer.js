b9c23ac279e505f857b6f928d07f767403ee1013
Lucas Kwenda <133110595+lucaskwenda@users.noreply.github.com>
2025-05-11 07:56:43 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 // btnChat.addEventListener('click', handleChatButtonClick)
 // Adicionar o evento de clique ao botão
-const base = '/https://lucaskwenda.github.io/Amanimoyo/';
+//const base = '/https://lucaskwenda.github.io/Amanimoyo/';
 const chatButton = document.querySelectorAll('.btn-chat');
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -28,13 +28,13 @@ document.addEventListener('DOMContentLoaded', function() {
   // Função para redirecionar para a página de login
   function redirectToLogin() {
     // Salva a página do chat como destino após o login
-    sessionStorage.setItem('redirectAfterLogin', `${base}chat/chatIA.html`);
+    sessionStorage.setItem('redirectAfterLogin', 'chat/chatIA.html');
     // Redireciona para a página de login
-    window.open(base + 'pages/login.html', '_self')
+       window.location.href = '../pages/login.html';
   }
   // Função para redirecionar para o chat
   function redirectToChat() {
-    window.open( base + 'chat/chatIA.html', '_self')
+    window.location.href = '../chat/chatIA.htmll';
         
   }
   
