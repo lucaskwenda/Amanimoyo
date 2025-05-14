b9343f772161109a94502d61043d31d0b07765b3
Lucas Kwenda <133110595+lucaskwenda@users.noreply.github.com>
2025-05-14 18:35:43 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -28,14 +28,14 @@ document.addEventListener('DOMContentLoaded', function() {
   // Função para redirecionar para a página de login
   function redirectToLogin() {
     // Salva a página do chat como destino após o login
-    sessionStorage.setItem('redirectAfterLogin', './chat/chatIA.html');
+    sessionStorage.setItem('redirectAfterLogin', '/chat/chatIA.html');
     // Redireciona para a página de login
-        history.pushState(null, '', '/pages/login.html');
-      // window.location.href = './pages/login.html';
+       // history.pushState(null, '', '/pages/login.html');
+      window.location.href = '/pages/login.html';
   }
   // Função para redirecionar para o chat
   function redirectToChat() {
-    window.location.href = './chat/chatIA.html';
+    window.location.href = '/chat/chatIA.html';
         
   }
   
