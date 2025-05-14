d68efd7dd84158f705a7c36479327cdcb1f937ca
Lucas Kwenda <133110595+lucaskwenda@users.noreply.github.com>
2025-05-14 19:38:19 +0100

    Update verification-page.js

--- js/verification-page.js
+++ js/verification-page.js
@@ -54,7 +54,7 @@
              successMessage.style.display = 'block';
              errorMessage.style.display = 'none';
              setTimeout(() => {
-                 window.location.href = '/Amanimoyo/pages/user-profile.html';
+                 window.location.href = '/pages/user-profile.html';
              }, 2000);
          } else {
              errorMessage.style.display = 'block';
